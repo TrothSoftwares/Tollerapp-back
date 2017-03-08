d110fce34baae32c78c49c72b5b241e1b44c4d7f
talhaqqq <talhaqqq@gmail.com>
2017-03-08 16:53:05 +0530

    after initializing git

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create(email:'local@admin.com',password:'password',role:'local')
+User.create(email:'super@admin.com',password:'password',role:'super')
+User.create(email:'group@admin.com',password:'password',role:'group')
+
